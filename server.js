807470f9f12138c5965b84598aa300100997b47f
Noel Nevarez <noel3740@gmail.com>
2019-02-05 17:53:40 -0600

    Adding userApiRoutes to the server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
+require("./routes/usersApiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
