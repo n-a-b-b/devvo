8e1513135d18d15ce2975af5a1dc86632f02adcf
Amena Ahmed <am.ahmed09@gmail.com>
2019-02-04 20:01:11 -0600

    created sequelize databases

---
+++ models/user_task.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  var UserTask = sequelize.define("UserTask", { 
+    completed: DataTypes.BOOLEAN,
+    dateDue: DataTypes.DATE, 
+    dateCompleted: DataTypes.DATE
+  });
+  
+  UserTask.associate = function (models){
+    UserTask.belongsTo(models.Task, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    UserTask.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }
+  return UserTask;
+};
+
