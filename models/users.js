1de71e388634f578b4ef2eda2250364e2a10dc1e
Noel Nevarez <noel3740@gmail.com>
2019-02-05 17:53:27 -0600

    Adding js tied to all the various pages.

--- models/users.js
+++ models/users.js
@@ -1,9 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", { 
     email: DataTypes.STRING,
-    profile_url: DataTypes.STRING, 
+    profileUrl: DataTypes.STRING, 
     name: DataTypes.STRING,
-    skill_level: DataTypes.STRING
+    skillLevel: DataTypes.STRING
   });
   User.associate = function (models){
     User.hasMany(models.UserTask, {
---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  //Check to see if the user is already authenticated and dispplay or hide fields. 
+  checkAuthentication();
+});
