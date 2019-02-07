f8ef5ad678571a2240984befee1f21e38456414c
Noel Nevarez <noel3740@gmail.com>
2019-02-07 11:36:46 -0600

    Ading user task route

---
+++ routes/userTasksApiRoutes.js
@@ -0,0 +1,35 @@
+var db = require("../models");
+
+module.exports = function (app) {
+
+  // Create a user tasks for a specific user id
+  app.post("/api/userTasks", function (req, res) {
+    const userId = req.body.userId;
+
+    //Grab all tasks from the task table
+    db.Task.findAll()
+      .then(function (dbTask) {
+
+        //Loop thru all the tasks and create an array of new user tasks
+        const newUserTasks = [];
+
+        dbTask.forEach(task => {
+
+          const userTask = {
+            UserId: userId,
+            TaskId: task.id
+          };
+
+          newUserTasks.push(userTask);
+        });
+
+        //Add the tasks to the userTask table
+        db.UserTask.bulkCreate(newUserTasks)
+          .then(function (dbUserTasks) {
+            res.json(dbUserTasks);
+          });        
+      });
+
+
+  });
+};
