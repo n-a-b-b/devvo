604c776e758d55d66de793b373f5a806069afb52
Noel Nevarez <noel3740@gmail.com>
2019-02-08 10:35:58 -0600

    Remove seeding sql files as they are no longer needed.

--- seedsCategories.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Categories(name) VALUES("Articles");
-INSERT INTO Categories(name) VALUES("Exercises");
-INSERT INTO Categories(name) VALUES("Videos");
-INSERT INTO Categories(name) VALUES("Networking");
