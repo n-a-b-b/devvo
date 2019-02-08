326a2edd110e0a6c47f05dd40e0ddd383f76fc15
Noel Nevarez <noel3740@gmail.com>
2019-02-08 15:36:21 -0600

    Adding logoff ability

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -38,7 +38,7 @@ function getCookie(cname) {
 }
 
 function deleteCookie (name) {
-  document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;";
 }
 
 function isAuthenticated() {
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
   //Check to see if the user is already authenticated and dispplay or hide fields. 
   checkAuthentication();
+
+  //Enable bootstrap popovers
+  $("[data-toggle='popover']").popover();
+
+  $(document).on("click", "#signOutButton", function() {
+    deleteCookie(authenticationCookieName);
+    location.href = "/";
+  });
 });
