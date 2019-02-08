a1baff3d175e8dc8da195a4549e35ac90aa6fe62
Amena Ahmed <am.ahmed09@gmail.com>
2019-02-08 11:27:50 -0600

    Deleted API Routes file.

--- public/js/successBoard.js
+++ public/js/successBoard.js
@@ -1,26 +1,4 @@
 
-// var pieData = [
-//   {
-//       value: 20,
-//       color:"#878BB6"
-//   },
-//   {
-//       value : 40,
-//       color : "#4ACAB4"
-//   },
-//   {
-//       value : 10,
-//       color : "#FF8153"
-//   },
-//   {
-//       value : 30,
-//       color : "#FFEA88"
-//   }
-// ];
-// // Get the context of the canvas element we want to select
-// var countries= document.getElementById("countries").getContext("2d");
-// new Chart(countries).Pie(pieData);
-
 // Our labels along the x-axis
 var weeks = ["January", "February", "March", "April"];
 // For drawing the lines
