9e6e7d89583666fafff33e54a0f996ab7891c832
Noel Nevarez <noel3740@gmail.com>
2019-02-11 15:56:30 -0600

    Formatting code and fixing error in heroku.

--- public/js/successBoard.js
+++ public/js/successBoard.js
@@ -6,38 +6,41 @@ $(document).ready(function () {
   }
 
   //LINE CHART-TOP 5 IN PROGRESS
-  
+
   //get number of users and store it in variable. 
   let currentNumUsers = 10;
   let userNumCompletedArr = new Array(currentNumUsers);
   // let numCompletedArr = new Array(35);
 
   $.get("/api/userTasks/all", function (data) {
-    
+
     console.log(data);
 
     let orderedSumsArr = new Array(currentNumUsers);
 
     //Create 2-D array to store number of completed tasks per user
-    for(let j=0; j<currentNumUsers; j++){
-      userNumCompletedArr[j]= new Array(currentNumUsers*35);
+    for (let j = 0; j < currentNumUsers; j++) {
+      userNumCompletedArr[j] = new Array(currentNumUsers * 35);
     }
     console.log(userNumCompletedArr);
-    
+
     //Initialize array to zero              
-    for(let p=0; p<currentNumUsers; p++){
-      for(let q=0; q<currentNumUsers*35; q++){
-        userNumCompletedArr[p][q]= 0;
+    for (let p = 0; p < currentNumUsers; p++) {
+      for (let q = 0; q < currentNumUsers * 35; q++) {
+        userNumCompletedArr[p][q] = 0;
       }
     }
 
     console.log(userNumCompletedArr);
 
     //How many complete per user 
-    for(let k = 0; k<=currentNumUsers; k++){
-      for(let l=0; l<=data.length-1; l++){
-        if(data[l].UserId === k+1 && data[l].completed === true){
-          userNumCompletedArr[k][l]=1; 
+    for (let k = 0; k <= currentNumUsers; k++) {
+      for (let l = 0; l <= data.length - 1; l++) {
+        if (data[l].UserId === k + 1 && data[l].completed === true) {
+          if (userNumCompletedArr[k] &&
+            userNumCompletedArr[k].length > l) {
+            userNumCompletedArr[k][l] = 1;
+          }
         }
       }
     }
@@ -45,20 +48,20 @@ $(document).ready(function () {
 
     //ALGORITHM TO FIND SUM OF TASKS COMPLETED PER USER
     let sumsCompletedArr = new Array(currentNumUsers);
-  
+
     //Create 2-D array to store *sum* of completed tasks per user
-    for(let t=0; t<currentNumUsers; t++){
-      sumsCompletedArr[t]= new Array(2);
+    for (let t = 0; t < currentNumUsers; t++) {
+      sumsCompletedArr[t] = new Array(2);
     }
-    
+
     console.log(sumsCompletedArr);
 
     //Loop through and get sum of completed tasks per user and store
     //in 2-D array. 
     let sum = 0;
-    for (let r=0; r<userNumCompletedArr.length; r++){
-      for(let s=0; s<(currentNumUsers*35); s++){
-        
+    for (let r = 0; r < userNumCompletedArr.length; r++) {
+      for (let s = 0; s < (currentNumUsers * 35); s++) {
+
         sum += userNumCompletedArr[r][s];
         console.log("sum in for loop = " + sum);
       }
@@ -69,57 +72,57 @@ $(document).ready(function () {
     console.log(sumsCompletedArr);
 
     //**Find 5 top performers**
-    
+
     //Store all sums in order in new array
     let sumHolder = 0;
     // let orderedSumsArr = new Array(currentNumUsers);
 
-    for (let t=0; t<sumsCompletedArr.length; t++){
-      sumHolder =   sumsCompletedArr[t][0];
-      console.log("sumHolder = "+ sumHolder);
-      orderedSumsArr[t]=(sumHolder);
+    for (let t = 0; t < sumsCompletedArr.length; t++) {
+      sumHolder = sumsCompletedArr[t][0];
+      console.log("sumHolder = " + sumHolder);
+      orderedSumsArr[t] = (sumHolder);
     }
     console.log(orderedSumsArr);
     console.log(orderedSumsArr.length);
-    
+
     //Order the sums greatest to least
     let sumHolder2 = 0;
     let swapped = false;
-    do{
+    do {
       swapped = false;
-      for(let u=orderedSumsArr.length; u>0; u--){
+      for (let u = orderedSumsArr.length; u > 0; u--) {
         console.log(" u = " + u);
         console.log("orderedSumsArr[u] = " + orderedSumsArr[u]);
-        console.log("orderedSumsArr[u-1] = " + orderedSumsArr[u-1]);
-        if(orderedSumsArr[u] >orderedSumsArr[u-1]){
-          sumHolder2 = orderedSumsArr[u-1];
-          orderedSumsArr[u-1]=orderedSumsArr[u];
-          orderedSumsArr[u]= sumHolder2;
+        console.log("orderedSumsArr[u-1] = " + orderedSumsArr[u - 1]);
+        if (orderedSumsArr[u] > orderedSumsArr[u - 1]) {
+          sumHolder2 = orderedSumsArr[u - 1];
+          orderedSumsArr[u - 1] = orderedSumsArr[u];
+          orderedSumsArr[u] = sumHolder2;
           swapped = true;
         }
         console.log(orderedSumsArr);
       }
       console.log(orderedSumsArr);
-    }while(swapped);
-    
+    } while (swapped);
+
     console.log("Now logging final sums array: ");
     console.log(orderedSumsArr);
 
     //Create 2-D array to store number of completed tasks per user
-    for(let x=0; x<orderedSumsArr.length; x++){
-      orderedSumsArr[x]= new Array(1);
+    for (let x = 0; x < orderedSumsArr.length; x++) {
+      orderedSumsArr[x] = new Array(1);
     }
     console.log(orderedSumsArr[0][0]);
-    
+
     //Match the top 5 scores to users
     let tempUser = 0;
-    let topFiveIdArr = []; 
-    for (let w=0; w<sumsCompletedArr.length; w++){
-      for (let x=0; x<orderedSumsArr.length; x++){
+    let topFiveIdArr = [];
+    for (let w = 0; w < sumsCompletedArr.length; w++) {
+      for (let x = 0; x < orderedSumsArr.length; x++) {
         console.log(sumsCompletedArr[w][0]);
         console.log(orderedSumsArr[x]);
-        if (sumsCompletedArr[w][0]===orderedSumsArr[x][0]){
-          tempUser = w+1;
+        if (sumsCompletedArr[w][0] === orderedSumsArr[x][0]) {
+          tempUser = w + 1;
           orderedSumsArr[x][1] = tempUser;
           console.log(orderedSumsArr[x][1]);
         }
@@ -208,7 +211,7 @@ $(document).ready(function () {
     });
 
   });
-  
+
 
 
 
@@ -218,49 +221,49 @@ $(document).ready(function () {
   let userObject = getUserObject();
   let completedNetworking = 0;
   let incompleteNetworking = 0;
-  let completedCoding= 0;
-  let incompleteCoding= 0;
-  let completedArticles= 0;
-  let incompleteArticles=0;
-  let completedVideos=0;
-  let incompleteVideos=0;
+  let completedCoding = 0;
+  let incompleteCoding = 0;
+  let completedArticles = 0;
+  let incompleteArticles = 0;
+  let completedVideos = 0;
+  let incompleteVideos = 0;
   let networkingArr = [];
   let codingArr = [];
   let articlesArr = [];
   let videosArr = [];
 
   $.get(`/api/userTasks/${userObject.userId}`, function (data) {
-   
-    for(let i = 0; i<data.length; i++){
 
-      if(data[i].Task.CategoryId === 1){
-        if(data[i].completed=== true){
+    for (let i = 0; i < data.length; i++) {
+
+      if (data[i].Task.CategoryId === 1) {
+        if (data[i].completed === true) {
           completedNetworking++;
-        }else{
-          
+        } else {
+
           incompleteNetworking++;
         }
       }
-      if(data[i].Task.CategoryId === 2){
-        if(data[i].completed=== true){
-          
+      if (data[i].Task.CategoryId === 2) {
+        if (data[i].completed === true) {
+
           completedCoding++;
-        }else{
-         
+        } else {
+
           incompleteCoding++;
         }
       }
-      if(data[i].Task.CategoryId === 3){
-        if(data[i].completed=== true){
+      if (data[i].Task.CategoryId === 3) {
+        if (data[i].completed === true) {
           completedArticles++;
-        }else{
+        } else {
           incompleteArticles++;
         }
       }
-      if(data[i].Task.CategoryId === 4){
-        if(data[i].completed=== true){
+      if (data[i].Task.CategoryId === 4) {
+        if (data[i].completed === true) {
           completedVideos++;
-        }else{
+        } else {
           incompleteVideos++;
         }
       }
@@ -278,7 +281,7 @@ $(document).ready(function () {
     codingArr.push(completedCoding, incompleteCoding);
     articlesArr.push(completedArticles, incompleteArticles);
     videosArr.push(completedVideos, incompleteVideos);
- 
+
     let pie1 = new Chart(document.getElementById("my-chart-pie1"), {
       type: "pie",
       data: {
