874421732bdffb0e79ad9da50e8f85727a93a660
Amena Ahmed <am.ahmed09@gmail.com>
2019-02-11 14:58:22 -0600

    Top 5 scores are logging in order, but line graph is not populated yet.

--- public/js/successBoard.js
+++ public/js/successBoard.js
@@ -5,23 +5,18 @@ $(document).ready(function () {
     return;
   }
 
-  //BAR CHART-TOP 5 IN PROGRESS
-  // Our labels along the x-axis
-  var weeks = ["January", "February", "March", "April"];
-  // For drawing the lines
-  var User1 = [2, 10, 30, 48];
-  var User2 = [2, 17, 29, 30];
-  var User3 = [3, 5, 10, 15];
-  var User4 = [10, 20, 25, 35];
-  var User5 = [5, 10, 15, 39];
+  //LINE CHART-TOP 5 IN PROGRESS
+  
   //get number of users and store it in variable. 
   let currentNumUsers = 10;
   let userNumCompletedArr = new Array(currentNumUsers);
   // let numCompletedArr = new Array(35);
 
   $.get("/api/userTasks/all", function (data) {
-    let orderedSumsArr = new Array(currentNumUsers);
+    
+    console.log(data);
 
+    let orderedSumsArr = new Array(currentNumUsers);
 
     //Create 2-D array to store number of completed tasks per user
     for(let j=0; j<currentNumUsers; j++){
@@ -53,7 +48,7 @@ $(document).ready(function () {
   
     //Create 2-D array to store *sum* of completed tasks per user
     for(let t=0; t<currentNumUsers; t++){
-      sumsCompletedArr[t]= new Array(1);
+      sumsCompletedArr[t]= new Array(2);
     }
     
     console.log(sumsCompletedArr);
@@ -106,81 +101,116 @@ $(document).ready(function () {
       }
       console.log(orderedSumsArr);
     }while(swapped);
+    
     console.log("Now logging final sums array: ");
     console.log(orderedSumsArr);
+
+    //Create 2-D array to store number of completed tasks per user
+    for(let x=0; x<orderedSumsArr.length; x++){
+      orderedSumsArr[x]= new Array(1);
+    }
+    console.log(orderedSumsArr[0][0]);
+    
+    //Match the top 5 scores to users
+    let tempUser = 0;
+    let topFiveIdArr = []; 
+    for (let w=0; w<sumsCompletedArr.length; w++){
+      for (let x=0; x<orderedSumsArr.length; x++){
+        console.log(sumsCompletedArr[w][0]);
+        console.log(orderedSumsArr[x]);
+        if (sumsCompletedArr[w][0]===orderedSumsArr[x][0]){
+          tempUser = w+1;
+          orderedSumsArr[x][1] = tempUser;
+          console.log(orderedSumsArr[x][1]);
+        }
+      }
+    }
+    console.log(topFiveIdArr);
+    //**Draw the Graph**
+    // Our labels along the x-axis
+    var weeks = ["January", "February", "March", "April"];
+    // For drawing the lines
+    var User1 = [2, 10, 30, 48];
+    var User2 = [2, 17, 29, 30];
+    var User3 = [3, 5, 10, 15];
+    var User4 = [10, 20, 25, 35];
+    var User5 = [5, 10, 15, 39];
+
+    var ctx = document.getElementById("my-chart-bar");
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: weeks,
+        datasets: [
+          {
+            data: User1,
+            label: "S.Rodriguez",
+            borderColor: "#3e95cd",
+            fill: false
+          },
+          {
+            data: User2,
+            label: "J.Flahive",
+            borderColor: "#DC143C",
+            fill: false
+          },
+          {
+            data: User3,
+            label: "M.Hertz",
+            borderColor: "#FF8C00",
+            fill: false
+          },
+          {
+            data: User4,
+            label: "B.Khan",
+            borderColor: "#FF00FF",
+            fill: false
+          },
+          {
+            data: User5,
+            label: "J.Hamed",
+            borderColor: "#9ACD32",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: "Top 5 Developers In Progress!",
+          position: "top",
+          fontSize: 20
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              ticks: {
+                display: true,
+                labelString: "Tasks Completed"
+              }
+            },
+            min: 0,
+            max: 50
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "Weeks"
+            },
+            ticks: {
+              min: 0,
+              max: 30,
+              stepSize: 1
+            }
+          }]
+        }
+      }
+    });
+
   });
   
 
-  var ctx = document.getElementById("my-chart-bar");
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: weeks,
-      datasets: [
-        {
-          data: User1,
-          label: "S.Rodriguez",
-          borderColor: "#3e95cd",
-          fill: false
-        },
-        {
-          data: User2,
-          label: "J.Flahive",
-          borderColor: "#DC143C",
-          fill: false
-        },
-        {
-          data: User3,
-          label: "M.Hertz",
-          borderColor: "#FF8C00",
-          fill: false
-        },
-        {
-          data: User4,
-          label: "B.Khan",
-          borderColor: "#FF00FF",
-          fill: false
-        },
-        {
-          data: User5,
-          label: "J.Hamed",
-          borderColor: "#9ACD32",
-          fill: false
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: "Top 5 Developers In Progress!",
-        position: "top",
-        fontSize: 20
-      },
-      scales: {
-        yAxes: [{
-          scaleLabel: {
-            ticks: {
-              display: true,
-              labelString: "Tasks Completed"
-            }
-          },
-          min: 0,
-          max: 50
-        }],
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: "Weeks"
-          },
-          ticks: {
-            min: 0,
-            max: 30,
-            stepSize: 1
-          }
-        }]
-      }
-    }
-  });
+
 
   //PIE CHARTS-SUCCESS PER CATEGORY
   //Retrieve the current data from db
@@ -236,15 +266,14 @@ $(document).ready(function () {
       }
     }
 
-    console.log("C1=" + completedNetworking);
-    console.log(typeof(completedNetworking));
-    console.log("IC1=" + incompleteNetworking);
-    console.log("C2=" + completedCoding);
-    console.log("IC2=" + incompleteCoding);
-    console.log("C3=" + completedArticles);
-    console.log("IC3=" + incompleteArticles);
-    console.log("C4=" + completedVideos);
-    console.log("IC4=" + incompleteVideos);
+    // console.log("C1=" + completedNetworking);
+    // console.log("IC1=" + incompleteNetworking);
+    // console.log("C2=" + completedCoding);
+    // console.log("IC2=" + incompleteCoding);
+    // console.log("C3=" + completedArticles);
+    // console.log("IC3=" + incompleteArticles);
+    // console.log("C4=" + completedVideos);
+    // console.log("IC4=" + incompleteVideos);
     networkingArr.push(completedNetworking, incompleteNetworking);
     codingArr.push(completedCoding, incompleteCoding);
     articlesArr.push(completedArticles, incompleteArticles);
